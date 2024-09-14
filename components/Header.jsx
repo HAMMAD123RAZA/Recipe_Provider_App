80628873b5d3fe3a5f7790f244365b21f3b000ea
Hammad Raza <01hammadraza@gmail.com>
2024-09-14 21:52:50 +0500

    'fewUpdation'

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,27 @@
 import { View, Text, Image, TextInput } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import img from '../assets/images/react-logo.png'
 import { Colors } from '@/constants/Colors'
 import { Ionicons } from '@expo/vector-icons'
+import { auth } from '@/firebase/Configs'
 
 const Header = () => {
+  const [Name, setName] = useState()
+  useEffect(()=>{
+    const user=auth.currentUser;
+    if (user) {
+        setName(user.displayName || 'Recipe Family')
+    }
+  
+  },[])
+  
   return (
     <View style={{backgroundColor:Colors.primary,borderBottomEndRadius:32,borderBottomLeftRadius:32}}  >
     <View className='flex-row gap-3 pt-2 px-5'  >
       <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaOZMkQV4Mb6VMHqIknnZdvriMOBetKD-a_w&s'}} borderRadius={22}  width={80} height={70}/>
       <View className='pt-2 ' >
       <Text className='text-gray-200 font-bold text-xl ' >Welcome</Text>
-      <Text className='text-gray-200 font-bold text-xl ' >To Recipes</Text>
+      <Text className='text-gray-200 font-bold text-xl ' >{Name}</Text>
       </View>
 
     </View>
